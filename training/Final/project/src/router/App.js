29bcfb9df27a252cfe97205605ef19421a215e09
JatinMangla <jatinmangla123@gmail.com>
2022-02-23 12:59:00 +0530

    final_test

---
+++ training/Final/project/src/router/App.js
@@ -0,0 +1,22 @@
+const express =require("express");
+const mongoose = require("mongoose")
+const url ='mongodb://localhost:27017/users'
+
+const app = express()
+mongoose.connect(url,{useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log('connected...')
+})
+
+app.use(express.json())
+
+const alienRouter = require('../dbCon/router/user')
+app.use('/user',alienRouter)
+
+app.listen(9100, () => {
+    console.log('Server started')
+})
+
+export default app;
